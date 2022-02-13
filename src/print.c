4b8a9d87bd094abe955af9efe6793ae6833c8edb
Alexdelia <alexandre.delille.57@gmail.com>
2022-02-13 16:38:55 +0100

    fix include

--- src/print.c
+++ src/print.c
@@ -6,48 +6,11 @@
 /*   By: adelille <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/12 12:24:54 by adelille          #+#    #+#             */
-/*   Updated: 2022/02/13 13:42:22 by adelille         ###   ########.fr       */
+/*   Updated: 2022/02/13 16:38:49 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/alcu.h"
-
-#include <stdio.h>
-
-/*void	print_map(t_map map)
-{
-	int j = 0;
-	int len = 0;
-	int diff = 0;
-	int highest_len = 0;
-
-	while (map.map[j]) // get the biggest line nb
-	{
-		len = map.map[j];
-		if (len > highest_len)
-			highest_len = len;
-		j++;
-	}
-	j = 0;
-	while (map.map[j])
-	{
-		len = map.map[j];
-		diff = highest_len - len;
-		while (diff > 0)
-		{
-			write(1, " ", 1);
-			diff--;
-		}
-		while (len > 0)
-		{
-			write(1, "| ", 2);
-			len--;
-		}
-		write(1, "\n", 1);
-		j++;
-	}
-	return ;
-}*/
 
 static void	print_arrow(size_t len, const size_t highest)
 {
